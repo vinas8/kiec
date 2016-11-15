e73972478fed42dc7d6213824c9cb01ae44018cf
Žilvinas Navickas <navickaszilvinas@gmail.com>
2016-11-15 10:54:32 +0200

    Add LessonException errors

--- src/AppBundle/Controller/CurrentLessonController.php
+++ src/AppBundle/Controller/CurrentLessonController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Exception\LessonException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -18,7 +19,13 @@ class CurrentLessonController extends Controller
      */
     public function resultsAction()
     {
-        $currentLesson = $this->get('app.lesson_service')->getCurrentLesson();
+        try {
+            $currentLesson = $this->get('app.lesson_service')->getCurrentLesson();
+        }
+        catch (LessonException $e) {
+            $this->addFlash('notice', $e->getMessage());
+            return $this->render('@App/current_lesson/errors.html.twig');
+        }
         return $this->render('@App/current_lesson/results.html.twig', [
             'currentLesson' => $currentLesson
         ]);
