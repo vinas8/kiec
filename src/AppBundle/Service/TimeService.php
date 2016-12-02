803a02f93be5dcf103fb47f7b07bfafb8faaae03
Žilvinas Navickas <navickaszilvinas@gmail.com>
2016-12-02 13:02:09 +0200

    Format by day of week

--- src/AppBundle/Service/TimeService.php
+++ src/AppBundle/Service/TimeService.php
@@ -13,7 +13,7 @@ class TimeService
     public function getCurrentTime()
     {
         $now = new \DateTime();
-        $now->format("Y-m-d H:m");
+        $now->format("l H:m");
         $now->setTimezone(new \DateTimeZone('Europe/Vilnius'));
         return $now;
     }
