f1f79f08ac01957c3c9e284f3d5d4543ad86a951
Žilvinas Navickas <navickaszilvinas@gmail.com>
2016-12-06 15:57:14 +0200

    Fix image location

--- src/AppBundle/GlobalConstants.php
+++ src/AppBundle/GlobalConstants.php
@@ -4,6 +4,6 @@ namespace AppBundle;
 
 final class GlobalConstants
 {
-    const PROFILE_IMAGE_LOCATION = 'images/profile/';
+    const PROFILE_IMAGE_LOCATION = '/images/profile/';
     const PROFILE_IMAGE_DEFAULT = GlobalConstants::PROFILE_IMAGE_LOCATION . 'default_user_image.png';
 }
