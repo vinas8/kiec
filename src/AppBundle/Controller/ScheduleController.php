c9ae5a5f78762177f166c64998d5bbffb9c96d06
Gedas Lukošius <gedas@lukosius.me>
2016-12-06 15:59:43 +0200

    Added schedule controller

---
+++ src/AppBundle/Controller/ScheduleController.php
@@ -0,0 +1,17 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class ScheduleController extends Controller
+{
+    /**
+     * @Route("/schedule", name="schedule")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Schedule:index.html.twig');
+    }
+}
