6ba32ed962138e7956ff543ef8ea46f4363f913b
Gedas Lukošius <gedas@lukosius.me>
2016-12-06 16:01:27 +0200

    Naming & added function to find user lessons from given date

--- src/AppBundle/Service/LessonService.php
+++ src/AppBundle/Service/LessonService.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Service;
 
 use AppBundle\Entity\Lesson;
+use AppBundle\Entity\User;
 use AppBundle\Exception\LessonException;
 use AppBundle\Repository\LessonRepository;
 use Doctrine\ORM\NonUniqueResultException;
@@ -12,24 +13,25 @@ class LessonService
     /**
      * @var LessonRepository
      */
-    private $repository;
-    private $time;
+    private $lessonRepository;
+    private $timeService;
 
     /**
      * LessonService constructor.
      *
-     * @param LessonRepository $repository
+     * @param LessonRepository $lessonRepository
+     * @param TimeService $timeService
      */
-    public function __construct(LessonRepository $repository, TimeService $time)
+    public function __construct(LessonRepository $lessonRepository, TimeService $timeService)
     {
-        $this->repository = $repository;
-        $this->time = $time;
+        $this->lessonRepository = $lessonRepository;
+        $this->timeService = $timeService;
     }
 
     public function getCurrentLesson()
     {
         try {
-            return $this->repository->findLessonByTime($this->time->getCurrentTime());
+            return $this->lessonRepository->findLessonByTime($this->timeService->getCurrentTime());
         } catch (NonUniqueResultException $e) {
             throw new LessonException("Jūsų pamokų laikai dubliuojasi, patikrinkite pamokų tvarkaraštį");
         } catch (NoResultException $e) {
@@ -45,7 +47,7 @@ class LessonService
      */
     public function getNext(Lesson $lesson)
     {
-        return $this->repository->findNextLessonById($lesson->getId());
+        return $this->lessonRepository->findNextLessonById($lesson->getId());
     }
 
     /**
@@ -56,6 +58,19 @@ class LessonService
      */
     public function getPrev(Lesson $lesson)
     {
-        return $this->repository->findPrevLessonById($lesson->getId());
+        return $this->lessonRepository->findPrevLessonById($lesson->getId());
+    }
+
+    /**
+     * Finds future lessons from current time
+     *
+     * @param  User $user
+     * @param  int $offset
+     * @param  int $limit
+     * @return array
+     */
+    public function getUserLessonsFromNow(User $user, $offset, $limit)
+    {
+        return $this->lessonRepository->findUserLessonsFromDate($user, $this->timeService->getCurrentTime(), $offset, $limit);
     }
 }
