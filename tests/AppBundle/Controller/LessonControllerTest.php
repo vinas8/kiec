821d49c3113014720ccfee6757547007810b79c6
Žilvinas Navickas <navickaszilvinas@gmail.com>
2017-05-17 19:03:53 +0300

    ignore

--- tests/AppBundle/Controller/LessonControllerTest.php
+++ tests/AppBundle/Controller/LessonControllerTest.php
@@ -2,19 +2,13 @@
 
 namespace Tests\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
+use \GuzzleHttp\Client;
 
 class LessonControllerTest extends WebTestCase
 {
+
     public function testShowPost()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/post/hello-world');
-
-        $this->assertGreaterThan(
-            0,
-            $crawler->filter('html:contains("Hello World")')->count()
-        );
+        $client = new Guzzle
     }
 }
